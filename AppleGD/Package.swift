f2a7bd23777d7f140c573a672465ea12cda19e40
stokatyan <tokat.shant@gmail.com>
2025-07-18 11:28:20 -0700

    Setup initial workflow for type creation

---
+++ AppleGD/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppleGD",
+    platforms: [.macOS(.v14), .iOS(.v16)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppleGD",
+            type: .dynamic,
+            targets: ["AppleGD"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AppleGD",
+            dependencies: [
+                "SwiftGodot",
+            ],
+            swiftSettings: [.unsafeFlags(["-suppress-warnings"])]
+        ),
+        .testTarget(
+            name: "AppleGDTests",
+            dependencies: ["AppleGD"]
+        ),
+    ]
+)
---
+++ AppleGD/Sources/AppleGD/GameKitGD.swift
@@ -0,0 +1,17 @@
+//
+//  GameCenter.swift
+//  AppleGD
+//
+//  Created by Shant Tokatyan on 7/18/25.
+//
+
+import SwiftGodot
+import GameKit
+
+@Godot
+class GameKitNode: Node {
+    @Callable
+    func postScore() {
+        return
+    }
+}

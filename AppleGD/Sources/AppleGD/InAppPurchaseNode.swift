9fe3b2d011d8420690861e83f010d5d2ca8055f7
stokatyan <tokat.shant@gmail.com>
2025-07-19 11:46:11 -0700

    Update to listen for transaction updates

--- AppleGD/Sources/AppleGD/InAppPurchaseNode.swift
+++ AppleGD/Sources/AppleGD/InAppPurchaseNode.swift
@@ -11,14 +11,80 @@ import StoreKit
 @Godot
 class InAppPurchaseNode: Node {
     
+    private let kPendingPurchaseSet = "kPendingPurchaseSet"
+    
+    private var idToProductMap = [String: Product]()
+    
+    /**
+     A signal that emits when a product purchase is completed.
+     - parameters:
+        - product: The id of the product that was successfuly purchased.
+        - verificationResult: `true` iff StoreKit was able to verify the purchase.
+     */
+    @Signal var didCompleteProductPurchase: SignalWithArguments<String, Bool>
+    
+    /**
+     A signal that emits when a product purchase is canceled.
+     - parameters:
+        - product: The id of the product purchase that was canceled.
+     */
+    @Signal var didCancelProductPurchase: SignalWithArguments<String>
+    
+    /**
+     A signal that emits when a prodcut purchase is pending, which may result in a future signal regarding the state of the purchase.
+     - parameters:
+        - product: The id of the product that is pending.
+     */
+    @Signal var didCreatePendingProductPurchase: SignalWithArguments<String>
+    
+    /**
+     A signal that emits when a product purchase failed.
+     */
+    @Signal var didFailToCompleteProductPurchase: SignalWithArguments<String>
+    
+    /**
+     A signal that emits when a product purchase reaches an unkown state.
+     */
+    @Signal var didReachUnkownStateInProductPurchase: SignalWithArguments<String>
+    
+    /**
+     A signal that emits when products fail to load.
+     - parameters:
+        - products: The array of productIds that failed to load.
+     */
     @Signal var didFailToLoadAppProducts: SignalWithArguments<[String]>
     
     /**
      A signal that emits when products are loaded.
      - parameters:
-        - products: A variant that holds an array of dictionaries representing the prodcut
+        - products: A variant that holds an array of dictionaries representing the product.
      */
     @Signal var didLoadAppProducts: SignalWithArguments<VariantArray>
+    
+    @Callable(autoSnakeCase: true)
+    func listenForTransactionUpdates() {
+        let didCompletePurchase = didCompleteProductPurchase
+        Task {
+            for await result in Transaction.updates {
+                do {
+                    let transaction = try result.payloadValue
+                    let productId = transaction.productID
+                    if getIsPurchasePending(id: productId) {
+                        setIsPurchasePending(id: productId, isPending: false)
+                        
+                        if transaction.revocationDate == nil {
+                            didCompletePurchase.emit(productId, true)
+                        }
+                    }
+                    
+                    await transaction.finish()
+                    print("Transaction update received:", transaction)
+                } catch {
+                    print("Transaction update failed:", error)
+                }
+            }
+        }
+    }
         
     @Callable(autoSnakeCase: true)
     func requestProducts(productIdentifiers: [String]) {
@@ -31,9 +97,9 @@ class InAppPurchaseNode: Node {
             }
             
             var gProducts = VariantArray()
-            
-            
             for product in appProducts {
+                idToProductMap[product.id] = product
+                
                 let gProduct = GProduct()
                 await gProduct.set(product: product)
                 gProducts.append(gProduct.toVariant())
@@ -41,9 +107,62 @@ class InAppPurchaseNode: Node {
             
             successSignal.emit(gProducts)
         }
-        
     }
     
+    @Callable(autoSnakeCase: true)
+    func purchaseProduct(productId: String) {
+        guard let product = idToProductMap[productId] else {
+            return
+        }
+        
+        let didCancelPurchase = didCancelProductPurchase
+        let didCompletePurchase = didCompleteProductPurchase
+        let didCreatePendingPurchase = didCreatePendingProductPurchase
+        let didFailToCompletePurchase = didFailToCompleteProductPurchase
+        let didReachUnkownState = didReachUnkownStateInProductPurchase
+        
+        Task {
+            do {
+                let result = await try product.purchase()
+                switch result {
+                case .success(let verificationResult):
+                    switch verificationResult {
+                    case .unverified:
+                        didCompletePurchase.emit(productId, false)
+                    case .verified:
+                        didCompletePurchase.emit(productId, true)
+                    }
+                case .userCancelled:
+                    didCancelProductPurchase.emit(productId)
+                case .pending:
+                    setIsPurchasePending(id: productId, isPending: true)
+                    didCreatePendingPurchase.emit(productId)
+                @unknown default:
+                    didReachUnkownState.emit(productId)
+                }
+            } catch {
+                didFailToCompletePurchase.emit(productId)
+            }
+        }
+    }
+    
+    private func getIsPurchasePending(id: String) -> Bool {
+        let array = UserDefaults.standard.stringArray(forKey: kPendingPurchaseSet) ?? []
+        return array.contains { $0 == id }
+    }
+    
+    private func setIsPurchasePending(id: String, isPending: Bool) {
+        let array = UserDefaults.standard.stringArray(forKey: kPendingPurchaseSet) ?? []
+        var set = Set(array)
+        if isPending {
+            set.insert(id)
+        } else {
+            set.remove(id)
+        }
+        
+        let updatedArray = Array(set)
+        UserDefaults.standard.set(set, forKey: kPendingPurchaseSet)
+    }
 }
 
 extension InAppPurchaseNode: @unchecked Sendable {}

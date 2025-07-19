c346cbd7713a3bc87965d8eb210772b409d0fe0f
stokatyan <tokat.shant@gmail.com>
2025-07-18 17:16:34 -0700

    Continue building out GameKitGD; Introduce UIKit import to enable presenting leaderboard

--- AppleGD/Sources/AppleGD/GameKitGD.swift
+++ AppleGD/Sources/AppleGD/GameKitGD.swift
@@ -10,8 +10,49 @@ import GameKit
 
 @Godot
 class GameKitNode: Node {
-    @Callable
-    func postScore() {
-        return
+    
+    private var player: GKPlayer?
+    private var leaderboardMap = [String: GKLeaderboard]()
+    
+    @Signal var didLoadLeaderboards: SignalWithArguments<[String]>
+    
+    @Callable(autoSnakeCase: true)
+    func getLeaderboards(ids: [String]) {
+        Task {
+            let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: ids)
+        }
+    }
+    
+    @Callable(autoSnakeCase: true)
+    func getLoadedPlayer() -> [String: String] {
+        var result = [String: String]()
+        
+        guard let player else {
+            return result
+        }
+        
+        result["gamePlayerID"] = player.gamePlayerID
+        result["displayName"] = player.displayName
+        result["alias"] = player.alias
+        result["playerID"] = player.playerID
+        result["guestIdentifier"] = player.guestIdentifier
+        
+        return result
+    }
+
+    @Callable(autoSnakeCase: true)
+    func showLeaderboard(leaderboardId: String) {
+        
+    }
+    
+    @Callable(autoSnakeCase: true)
+    func submitScore(score: Int, context: Int, leaderboardIds: [String]) {
+        guard let player else {
+            return
+        }
+
+        GKLeaderboard.submitScore(score, context: context, player: player, leaderboardIDs: leaderboardIds) { error in
+            return
+        }
     }
 }
---
+++ AppleGD/Sources/AppleGD/ViewControllerPresenter.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerPresenter.swift
+//  AppleGD
+//
+//  Created by Shant Tokatyan on 7/18/25.
+//
+
+import UIKit
+
+public class ViewControllerPresenter {
+    public static func present(_ viewController: UIViewController, animated: Bool = true) {
+        guard let topVC = topViewController() else {
+            print("No view controller to present from")
+            return
+        }
+        topVC.present(viewController, animated: animated, completion: nil)
+    }
+
+    private static func topViewController(base: UIViewController? = UIApplication.shared.connectedScenes
+        .compactMap { ($0 as? UIWindowScene)?.keyWindow }
+        .first?.rootViewController) -> UIViewController? {
+
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+
+        if let tab = base as? UITabBarController,
+           let selected = tab.selectedViewController {
+            return topViewController(base: selected)
+        }
+
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+
+        return base
+    }
+}

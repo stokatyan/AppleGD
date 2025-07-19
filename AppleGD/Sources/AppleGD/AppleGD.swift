c3c35d9092a2dc1c891afa74813fe7b506cba487
stokatyan <tokat.shant@gmail.com>
2025-07-19 00:17:41 -0700

    ready to implement in app purchase node

--- AppleGD/Sources/AppleGD/AppleGD.swift
+++ AppleGD/Sources/AppleGD/AppleGD.swift
@@ -3,4 +3,4 @@
 
 import SwiftGodot
 
-#initSwiftExtension(cdecl: "swift_entry_point", types: [GameKitNode.self])
+#initSwiftExtension(cdecl: "swift_entry_point", types: [GameKitNode.self, InAppPurchaseNode.self])
---
+++ AppleGD/Sources/AppleGD/InAppPurchaseNode.swift
@@ -0,0 +1,14 @@
+//
+//  InAppPurchaseNode.swift
+//  AppleGD
+//
+//  Created by Shant Tokatyan on 7/19/25.
+//
+
+import SwiftGodot
+import StoreKit
+
+@Godot
+class InAppPurchaseNode: Node {
+    
+}
